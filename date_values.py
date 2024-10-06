510a9bccd1e5226ce98339dd0bd7aebc1a54cdb5
Thunder8110 <44309219+Thunder8110@users.noreply.github.com>
2024-10-06 12:53:27 +0900

    Fix expressions of date variables

--- date_values.py
+++ date_values.py
@@ -5,8 +5,8 @@ def get_date_values(from_date: int, to_date: int) -> tuple[int, int, int]:
   to_dt = datetime.datetime.fromtimestamp(to_date)
   delta = to_dt - from_dt
   day = delta.days
-  hour = delta.seconds // 3600
-  minute = (delta.seconds % 3600) // 60
+  hour = day * 24 + delta.seconds // 3600
+  minute = day * 24 * 60 + delta.seconds // 60
   return day, hour, minute
   
 def get_date_values_to_now(from_date: int) -> tuple[int, int, int]:
